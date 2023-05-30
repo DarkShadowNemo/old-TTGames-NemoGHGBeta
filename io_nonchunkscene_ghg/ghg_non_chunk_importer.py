bc26357b1e87eddbd78ca7cd6fa557217f38a97c
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-05-30 13:23:08 -0700

    Removes unwanted faces and append extra lost faces using remove and append list, still needs progress

--- io_nonchunkscene_ghg/ghg_non_chunk_importer.py
+++ io_nonchunkscene_ghg/ghg_non_chunk_importer.py
@@ -108,13 +108,58 @@ def WholeMeshGHGOne(f, vertices=[], faces=[], uvs=[],fa=-1,fb=0,fc=1,fa_=-3,fb_=
                 fb += 1
                 fc += 1
                 faces.append([fa,fb,fc])
-            if len(faces) == 391:
+            if len(vertices) == 595:
                 #Anemone
                 #https://stackoverflow.com/questions/51481077/python-removing-an-item-from-a-list-in-the-if-statement-condition
                 if faces.remove([173,174,175]):
                     pass
                 elif faces.remove([174,175,176]):
                     pass
+                elif faces.remove([244,245,246]):
+                    pass
+                elif faces.remove([243,244,245]):
+                    pass
+                elif faces.remove([208,209,210]):
+                    pass
+                elif faces.remove([209,210,211]):
+                    pass
+                elif faces.remove([278,279,280]):
+                    pass
+                elif faces.remove([279,280,281]):
+                    pass
+                elif faces.remove([103,104,105]):
+                    pass
+                #elif faces.remove([103,104,105]):
+                    #pass
+                elif faces.remove([104,105,106]):
+                    pass
+                elif faces.remove([138,139,140]):
+                    pass
+                elif faces.remove([139,140,141]):
+                    pass
+                elif faces.remove([383,384,385]):
+                    pass
+                elif faces.remove([384,385,386]):
+                    pass
+                elif faces.remove([313,314,315]):
+                    pass
+                elif faces.remove([314,315,316]):
+                    pass
+                elif faces.remove([349,350,351]):
+                    pass
+                elif faces.remove([348,349,350]):
+                    pass
+                elif faces.remove([34,35,36]):
+                    pass
+                elif faces.remove([33,34,35]):
+                    pass
+                elif faces.remove([68,69,70]):
+                    pass
+                elif faces.remove([69,70,71]):
+                    pass
+                elif faces.append([571,575,576]):
+                    pass
+                
             
     mesh = bpy.data.meshes.new("dragonjan")
     object = bpy.data.objects.new("dragonjan", mesh)
