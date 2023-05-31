57b91cb9e870e7da3556d4b8708875d64ed4e447
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-05-31 05:01:31 -0700

    Update __init__.py

--- io_nonchunkscene_ghg/__init__.py
+++ io_nonchunkscene_ghg/__init__.py
@@ -4,9 +4,8 @@ bl_info = {
 	'version'		: (0, 2, 6),
 	'blender'		: (3, 0, 0),
 	'location'		: 'File > Import',
-	'description'           : 'Import GHG one mesh chunk makes it easier',
-	'category'		: 'Non-Chunk-Importer',
-        'warning'               : 'Export is missing',
+	'description'           : 'Import GHG mesh chunk',
+	'category'		: 'Non-Chunk-Importer and Exporter',
 }
 import os
 import bpy
@@ -14,7 +13,8 @@ import importlib
 from bpy.props import CollectionProperty, StringProperty, BoolProperty, EnumProperty, FloatProperty, IntProperty
 from bpy_extras.io_utils import ImportHelper, ExportHelper
 
-from.import ghg_non_chunk_importer
+from.import ghg_non_chunk_importer, ghg_non_chunk_exporter
+import sys
 
 class ImportNonChunkGHG(bpy.types.Operator, ImportHelper):
         bl_idname  = 'import_non_chunk.ghg'
@@ -28,21 +28,57 @@ class ImportNonChunkGHG(bpy.types.Operator, ImportHelper):
         )
         directory: StringProperty()
         filter_glob: StringProperty(default = '*.ghg', options = {'HIDDEN'})
+
+        GHG_Meshes : IntProperty(name="GHG Mesh")
         def execute(self, context):
+                from .nemo import pearl
                 paths = [os.path.join(self.directory, name.name) for name in self.files]
                 if not paths: paths.append(self.filepath)
                 importlib.reload(ghg_non_chunk_importer)
-                for path in paths: ghg_non_chunk_importer.NonParseGHG(path)
+                for path in paths: ghg_non_chunk_importer.NonParseGHG(path, GHG_Meshes = self.GHG_Meshes)
                 return {'FINISHED'}
+
+class ExportNonChunkGHG(bpy.types.Operator, ExportHelper):
+        bl_idname  = 'export_non_chunk.ghg'
+        bl_label   = 'Export Non Chunk GHG'
+        bl_options = {'UNDO'}
+        filename_ext = '.ghg'
+        files: CollectionProperty(
+                name	    = 'File path',
+                description = 'File path used for finding the GHG file without chunks.',
+                type	    = bpy.types.OperatorFileListElement
+        )
+
+        appendGHG : BoolProperty(name="append 0x030100010380")
+
+        noappendGHG : BoolProperty(name="no append 0x030100010380")
+
+        directory: StringProperty()
+        filter_glob: StringProperty(default = '*.ghg', options = {'HIDDEN'})
+
+        def execute(self, context):
+            importlib.reload(ghg_non_chunk_exporter)
+            ghg_non_chunk_exporter.WritingEditGHG(self.filepath, appendGHG = self.appendGHG, noappendGHG = self.noappendGHG)
+            return {"FINISHED"}
+
+        
+        
 
         
 	
 def menu_func_import(self, context):
         self.layout.operator(ImportNonChunkGHG.bl_idname, text='GHG Non Chunk Importer (.ghg)')
+
+def menu_func_export(self, context):
+        self.layout.operator(ExportNonChunkGHG.bl_idname, text='GHG Non Chunk Exporter (.ghg)')
 def register():
         bpy.utils.register_class(ImportNonChunkGHG)
+        bpy.utils.register_class(ExportNonChunkGHG)
         bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+        bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 def unregister():
         bpy.utils.unregister_class(ImportNonChunkGHG)
+        bpy.utils.unregister_class(ExportNonChunkGHG)
         bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+        bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
 if __name__ == '__main__': register()
