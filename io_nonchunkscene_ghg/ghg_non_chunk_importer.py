c663ae70de323da86879c7d02d2fedb7f44f84ff
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-06-04 20:32:09 -0700

    might need help aligning ghg correct roll bones with maths and connect bones with weights

--- io_nonchunkscene_ghg/ghg_non_chunk_importer.py
+++ io_nonchunkscene_ghg/ghg_non_chunk_importer.py
@@ -1,8 +1,61 @@
 from struct import pack, unpack
 import os
 import bpy
+import mathutils
+import math
 
-def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=1,fc=0):
+def align_roll( vec, vecz, tarz ):
+
+    sine_roll = vec.normalized().dot(vecz.normalized().cross(tarz.normalized()))
+
+    if 1 < abs(sine_roll):
+        sine_roll /= abs(sine_roll)
+            
+    if 0 < vecz.dot( tarz ):
+        return math.asin( sine_roll )
+        
+    elif 0 < sine_roll:
+        return -math.asin( sine_roll ) + math.pi
+        
+    else:
+        return -math.asin( sine_roll ) - math.pi
+
+def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=0, fc=1, bones=[]):
+    coll = bpy.context.collection
+    skel = bpy.data.armatures.new('GHG Skeleton')
+    arma = bpy.data.objects.new('GHG Armature', skel)
+    coll.objects.link(arma)
+    bpy.context.view_layer.objects.active = arma
+    bpy.ops.object.mode_set(mode = 'EDIT')
+    f.seek(24,1)
+    BoneCount = unpack("<I", f.read(4))[0]
+    f.seek(4,1)
+    BoneEndSizeData = unpack("<I", f.read(4))[0]
+    f.seek(BoneEndSizeData-36,1)
+    for i in range(BoneCount):
+        f.seek(112,1)
+        bonex = unpack("<f", f.read(4))[0]
+        boney = unpack("<f", f.read(4))[0]
+        bonez = unpack("<f", f.read(4))[0]
+        f.seek(4,1)
+        bones.append([bonex,boney,bonez])
+        bone = skel.edit_bones.new("ghg bone")
+        #todo clean bones and align roll
+        bone.head = (
+            +bonex,
+            +boney,
+	    +bonez,
+            )
+        bone.tail = (
+            bone.head[0],
+            bone.head[1],
+            bone.head[2] + 0.03,
+        )
+        matrix_ = mathutils.Matrix().transposed()
+    bpy.ops.object.mode_set(mode = 'OBJECT')
+    f.seek(0)
+    
+        
     ChunkRead = f.read()
     f.seek(0)
     for i in range(len(ChunkRead)):
@@ -17,11 +70,10 @@ def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=1,fc=0):
                 vz = unpack("<f", f.read(4))[0]
                 nz = unpack("<f", f.read(4))[0]
                 vertices.append([vx,vy,vz])
-                
             for i in range(vertexCount-2):
-                fa+=1
-                fb+=1
-                fc+=1
+                fa +=1
+                fb +=1
+                fc +=1
                 faces.append([fa,fb,fc])
         elif Chunk == b"\x03\x02\x00\x01":
             f.seek(2,1)
@@ -34,7 +86,6 @@ def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=1,fc=0):
                 nz = unpack("<h", f.read(2))[0] / 4096.0
                 f.seek(8,1)
                 vertices.append([vx,vy,vz])
-
             for i in range(vertexCount//2-2):
                 fa +=1
                 fb +=1
@@ -64,17 +115,16 @@ def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=1,fc=0):
     object = bpy.data.objects.new("dragonjan", mesh)
     mesh.from_pydata(vertices, [], faces)
     bpy.context.collection.objects.link(object)
-
-    for fac in mesh.polygons:
-            fac.use_smooth = True
                 
 
 
 
-def NonParseGHG(filepath, GHG_Meshes=False):
+def NonParseGHG(filepath, GHG_Meshes=False, GHG_MESH_SEP=1):
     with open(filepath, "rb") as f:
         if GHG_Meshes:
-            GHG_read(f, vertices=[], faces=[], fa=-1, fb=1,fc=0)
+            GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=0, fc=1, bones=[])
+        if GHG_MESH_SEP == 2:
+            GHG_seperate_readInt_two(f, vertices=[], faces=[], normals=[])
                 
         
                 
