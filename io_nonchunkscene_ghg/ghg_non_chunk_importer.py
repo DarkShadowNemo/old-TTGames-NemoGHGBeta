bfcb4a53bedad4c6a7e5b4050f892dd381ef0516
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-06-07 06:57:49 -0700

    FINALLY TRIANGLES didn't crash, restarting it again now

--- io_nonchunkscene_ghg/ghg_non_chunk_importer.py
+++ io_nonchunkscene_ghg/ghg_non_chunk_importer.py
@@ -4,61 +4,7 @@ import bpy
 import mathutils
 import math
 
-
-#WHAT THE UVS ARE GOING STRANGE PATTERNS WITH BYTES???
-#need help on this
-def GHG_seek_uvs_tri(f, seek_=0, uvs=[]):
-    obdata = bpy.context.object.data
-    f.seek(seek_,1)
-    uvcount = unpack("B", f.read(1))[0]
-    f.seek(1,1)
-    for i in range(int(uvcount*2)):
-        uvx = unpack("B", f.read(1))[0] / 255.0
-        uvy = unpack("B", f.read(1))[0] / 255.0
-        uvs.append([uvx,uvy])
-    uv_tex = obdata.uv_layers.new()
-    uv_layer = obdata.uv_layers[0].data
-    vert_loops = {}
-    for l in obdata.loops:
-        vert_loops.setdefault(l.vertex_index, []).append(l.index)
-    for i, coord in enumerate(uvs):
-        for li in vert_loops[i]:
-            uv_layer[li].uv = coord
-
-def GHG_seek_indivitual_triangles(f, seek_= 0, normals=[], vertices=[], faces=[], fa=-1, fb=0, fc=1):
-    f.seek(seek_,1)
-    vertexCount = unpack("B", f.read(1))[0]
-    f.seek(1,1)
-    for i in range(vertexCount//2):
-        vx = unpack("<h", f.read(2))[0] / 4096.0
-        vy = unpack("<h", f.read(2))[0] / 4096.0
-        vz = unpack("<h", f.read(2))[0] / 4096.0
-        nz = unpack("<h", f.read(2))[0] / 4096.0
-        f.seek(8,1)
-        vertices.append([vx,vy,vz])
-        normals.append([0,0,1])
-    for i in range(vertexCount//2-2):
-        #faces = ([i+i%2],[i-i%2+1],[i+2])
-        fa += 1
-        fb += 1
-        fc += 1
-        faces.append([fa,fb,fc])
-            
-    mesh = bpy.data.meshes.new("dragonjan")
-    mesh.from_pydata(vertices, [], faces)
-    object = bpy.data.objects.new("dragonjan", mesh)
-    bpy.context.collection.objects.link(object)
-    bpy.data.materials.new(name="dragonjan_materials")
-            
-    for fac in mesh.polygons:
-        fac.use_smooth = True
-
-    vindex = 0
-    for vertex in mesh.vertices:
-        vertex.normal = normals[vindex]
-        vindex += 1
-
-def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=0, fc=1, bones=[]):
+def GHG_read(f, vertices=[], faces=[], normals=[], fa=-2, fb=-1, fc=0, bones=[]):
     coll = bpy.context.collection
     skel = bpy.data.armatures.new('GHG Skeleton')
     arma = bpy.data.objects.new('GHG Armature', skel)
@@ -108,10 +54,21 @@ def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=0, fc=1, bones=[]):
                 vz = unpack("<f", f.read(4))[0]
                 nz = unpack("<f", f.read(4))[0]
                 vertices.append([vx,vy,vz])
-            for i in range(vertexCount-2):
-                fa +=1
-                fb +=1
-                fc +=1
+        elif Chunk == b"\x03\x02\x00\x01":
+            f.seek(2,1)
+            vertexCount = unpack("B", f.read(1))[0]
+            clump = unpack("B", f.read(1))[0]
+            for i in range(vertexCount//2):
+                vx = unpack("<h", f.read(2))[0] / 4096.0
+                vy = unpack("<h", f.read(2))[0] / 4096.0
+                vz = unpack("<h", f.read(2))[0] / 4096.0
+                nz = unpack("<h", f.read(2))[0] / 4096.0
+                f.seek(8,1)
+                vertices.append([vx,vy,vz])
+            for i in range(vertexCount//5-2):
+                fa +=1 * 3
+                fb +=1 * 3
+                fc +=1 * 3
                 faces.append([fa,fb,fc])
         elif Chunk == b"\x04\x02\x00\x01":
             f.seek(2,1)
@@ -127,10 +84,10 @@ def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=0, fc=1, bones=[]):
                 vzn = unpack("<f", f.read(4))[0]
                 nzx = unpack("<f", f.read(4))[0]
                 vertices.append([vx,vy,vz])
-            for i in range(vertexCount//2-2):
-                fa +=1
-                fb +=1
-                fc +=1
+            for i in range(vertexCount//5-2):
+                fa +=1*3
+                fb +=1*3
+                fc +=1*3
                 faces.append([fa,fb,fc])
 
     mesh = bpy.data.meshes.new("dragonjan")
@@ -144,7 +101,7 @@ def GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=0, fc=1, bones=[]):
 def NonParseGHG(filepath, GHG_Meshes=False, GHG_MESH_SEP=1, seek__=0, seek_uv=0, GHG_MESH_SEP_UV=1):
     with open(filepath, "rb") as f:
         if GHG_Meshes:
-            GHG_read(f, vertices=[], faces=[], normals=[], fa=-1, fb=0, fc=1, bones=[])
+            GHG_read(f, vertices=[], faces=[], normals=[], fa=-2, fb=-1, fc=0, bones=[])
         if GHG_MESH_SEP == 2:
             GHG_seek_indivitual_triangles(f, seek_= seek__, normals=[], vertices=[], faces=[], fa=-1, fb=0, fc=1)
         if GHG_MESH_SEP_UV == 2:
