710f22e8c3e3361da74905d264488ca8b0477608
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2023-06-07 14:32:43 -0700

    add 4 ttgames support and a custom support

--- io_nonchunkscene_ghg/ghg_non_chunk_importer.py
+++ io_nonchunkscene_ghg/ghg_non_chunk_importer.py
@@ -4,7 +4,7 @@ import bpy
 import mathutils
 import math
 
-def GHG_read(f, vertices=[], edges=[], normals=[], ea=-1, eb=0, bones=[]):
+def read_att(f, vertices=[], faces=[], fa=-1, fb=0, fc=1, normals=[], bones=[]):
     coll = bpy.context.collection
     skel = bpy.data.armatures.new('GHG Skeleton')
     arma = bpy.data.objects.new('GHG Armature', skel)
@@ -54,21 +54,30 @@ def GHG_read(f, vertices=[], edges=[], normals=[], ea=-1, eb=0, bones=[]):
                 vz = unpack("<f", f.read(4))[0]
                 nz = unpack("<f", f.read(4))[0]
                 vertices.append([vx,vy,vz])
+                normals.append([0,0,1])
+            for i in range(vertexCount-2):
+                fa += 1
+                fb += 1
+                fc += 1
+                faces.append([fa,fb,fc])
         elif Chunk == b"\x03\x02\x00\x01":
             f.seek(2,1)
-            vertexCount = unpack("B", f.read(1))[0]
+            vertexCount = unpack("B", f.read(1))[0]//2
             clump = unpack("B", f.read(1))[0]
-            for i in range(vertexCount//2):
+            for i in range(vertexCount):
                 vx = unpack("<h", f.read(2))[0] / 4096.0
                 vy = unpack("<h", f.read(2))[0] / 4096.0
                 vz = unpack("<h", f.read(2))[0] / 4096.0
                 nz = unpack("<h", f.read(2))[0] / 4096.0
                 f.seek(8,1)
+                normals.append([0,0,1])
                 vertices.append([vx,vy,vz])
-            for i in range(vertexCount//2-2):
-                ea +=1
-                eb +=1
-                edges.append([ea,eb])
+            for i in range(vertexCount-2):
+                fa += 1
+                fb += 1
+                fc += 1
+                faces.append([fa,fb,fc])
+                
         elif Chunk == b"\x04\x02\x00\x01":
             f.seek(2,1)
             vertexCount = unpack("B", f.read(1))[0] // 2
@@ -78,32 +87,37 @@ def GHG_read(f, vertices=[], edges=[], normals=[], ea=-1, eb=0, bones=[]):
                 vy = unpack("<f", f.read(4))[0]
                 vz = unpack("<f", f.read(4))[0]
                 nz = unpack("<f", f.read(4))[0]
-                vxn = unpack("<f", f.read(4))[0]
-                vyn = unpack("<f", f.read(4))[0]
-                vzn = unpack("<f", f.read(4))[0]
-                nzx = unpack("<f", f.read(4))[0]
+                f.seek(16,1)
                 vertices.append([vx,vy,vz])
-            for i in range(vertexCount//2-2):
-                ea +=1
-                eb +=1
-                edges.append([ea,eb])
+            for i in range(vertexCount-2):
+                fa += 1
+                fb += 1
+                fc += 1
+                faces.append([fa,fb,fc])
+                normals.append([0,0,1])
 
     mesh = bpy.data.meshes.new("dragonjan")
     object = bpy.data.objects.new("dragonjan", mesh)
-    mesh.from_pydata(vertices, edges, [])
+    mesh.from_pydata(vertices, [], faces)
     bpy.context.collection.objects.link(object)
+    
+
                 
 
 
 
-def NonParseGHG(filepath, GHG_Meshes=False, GHG_MESH_SEP=1, seek__=0, seek_uv=0, GHG_MESH_SEP_UV=1):
+def NonParseGHG(filepath, LEGO_STAR_WARS=1, HAVEN_CALL_OF_THE_KING=1, CRASH_BANDICOOT_THE_WRATH_OF_CORTEX=1, FINDING_NEMO=1, CUSTOM_=1):
     with open(filepath, "rb") as f:
-        if GHG_Meshes:
-            GHG_read(f, vertices=[], edges=[], normals=[], ea=-1, eb=-0, bones=[])
-        if GHG_MESH_SEP == 2:
-            GHG_seek_indivitual_triangles(f, seek_= seek__, normals=[], vertices=[], faces=[], fa=-1, fb=0, fc=1)
-        if GHG_MESH_SEP_UV == 2:
-            GHG_seek_uvs_tri(f, seek_=seek_uv, uvs=[])
+        if LEGO_STAR_WARS == 1:
+            read_att(f, vertices=[], faces=[], fa=-1, fb=0, fc=1, normals=[], bones=[])
+        if HAVEN_CALL_OF_THE_KING == 1:
+            pass
+        if CRASH_BANDICOOT_THE_WRATH_OF_CORTEX == 1:
+            pass
+        if FINDING_NEMO == 1:
+            pass
+        if CUSTOM_ == 1:
+            pass
                 
         
                 
