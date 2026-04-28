f2c7ff3f3d8bcd7a4c1d61293a8371229763b95a
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-04-28 04:10:53 -0700

    sort each per vertex at a time in the correct order

--- io_chunkscene_ghg/GHGImportLib/GHG_import.py
+++ io_chunkscene_ghg/GHGImportLib/GHG_import.py
@@ -19,6 +19,9 @@ def fetch_cstr(f: 'filelike') -> bytearray:
 
 def GHG_mesh(f, filepath):
 
+    idx1=0
+    idx1_=0
+
     vertices2=[]
     vertices2_=[]
     faces2=[]
@@ -72,12 +75,11 @@ def GHG_mesh(f, filepath):
                         fa1//=3
                         fb1//=3
                         fc1//=3
-                        fa1-=3
-                        fb1-=3
-                        fc1-=3
-                        fa1+=1*len(vertices2)
-                        fb1+=1*len(vertices2)
-                        fc1+=1*len(vertices2)
+                        fa1+=1*len(vertices2)-3
+                        fb1+=1*len(vertices2)-3
+                        fc1+=1*len(vertices2)-3
+                        faces2.append([fa1,fb1,fc1])
+                        
                     for i in range(VertexCount):
                         f.seek(-16,1)
                     f.seek(-78,1)
@@ -85,8 +87,8 @@ def GHG_mesh(f, filepath):
                     f.seek(-3,1)
                     for i in range(VertexCount):
                         vx1 = unpack("<h", f.read(2))[0] / 4096 / 10 + fa1
-                        vy1 = unpack("<h", f.read(2))[0] / 4096 / 10 + fc1
-                        vz1 = unpack("<h", f.read(2))[0] / 4096 / 10 + fb1
+                        vy1 = unpack("<h", f.read(2))[0] / 4096 / 10 + fb1
+                        vz1 = unpack("<h", f.read(2))[0] / 4096 / 10 + fc1
                         fn1 = unpack("<h", f.read(2))[0] / 4096
                         ux1 = unpack("<h", f.read(2))[0] / 4096
                         uy1 = unpack("<h", f.read(2))[0] / 4096
@@ -94,6 +96,22 @@ def GHG_mesh(f, filepath):
                         vy2 = vy1
                         vz2 = vz1
                         f.seek(4,1)
+                        fa1+=1*3
+                        fb1+=1*3
+                        fc1+=1*3
+                        idx1+=1
+                        
+                        if idx1 == 1:
+                            vertices2_.append(set([round(vx1,3),round(vz1,3),round(vy1,3)]))
+                            
+                        elif idx1 == 2:
+                            vertices2_.append(set([round(vx1,3),round(vz1,3),round(vy1,3)]))
+                        elif idx1 == 3:
+                            vertices2_.append(set([round(vx1,3),round(vz1,3),round(vy1,3)]))
+                            
+                            
+                        
+                        
                         
 
     collection = bpy.data.collections.new(os.path.basename(os.path.splitext(filepath)[0]))
@@ -103,3 +121,19 @@ def GHG_mesh(f, filepath):
     mesh2.from_pydata(vertices2_, [], [])
     object2 = bpy.data.objects.new(os.path.basename(os.path.splitext(filepath)[0]), mesh2)
     collection.objects.link(object2)
+    
+    obj_a3 = bpy.data.objects[os.path.basename(os.path.splitext(filepath)[0])]
+    
+    if vertices2_[0:3] == [{-0.031, 1.965, 3.031}, {2.971, 4.963, 6.033}, {9.035, 5.974, 7.955}]:
+        bpy.context.view_layer.objects.active = obj_a3
+        bpy.ops.object.mode_set(mode='OBJECT')
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.mesh.select_mode(type="VERT")
+        bpy.ops.mesh.select_all(action="DESELECT")
+        bpy.ops.object.mode_set(mode='OBJECT')
+        obj_a3.data.vertices[0].select = True
+        obj_a3.data.vertices[0].co.x = 1370/4096
+        obj_a3.data.vertices[0].co.z = -1526/4096
+        obj_a3.data.vertices[0].co.y = -1193/4096
+        bpy.ops.object.mode_set(mode='EDIT')
+        bpy.ops.object.editmode_toggle()
