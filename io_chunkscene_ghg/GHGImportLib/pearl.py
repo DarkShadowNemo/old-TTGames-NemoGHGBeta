f2dd5e2bb2702093f1327ed0efdb3546019532d5
DarkShadowNemo <119450522+DarkShadowNemo@users.noreply.github.com>
2026-04-28 06:35:49 -0700

    quote things out

--- io_chunkscene_ghg/GHGImportLib/pearl.py
+++ io_chunkscene_ghg/GHGImportLib/pearl.py
@@ -10,7 +10,7 @@ def pearl_(filepath):
     bpy.ops.mesh.select_all(action="DESELECT")
     bpy.ops.object.mode_set(mode='OBJECT')
     obj_a3.data.vertices[0].select = True
-    obj_a3.data.vertices[0].co.x = 1370/4096
+    """obj_a3.data.vertices[0].co.x = 1370/4096
     obj_a3.data.vertices[0].co.z = -1526/4096
     obj_a3.data.vertices[0].co.y = -1193/4096
     obj_a3.data.vertices[1].select = True
@@ -20,7 +20,7 @@ def pearl_(filepath):
     obj_a3.data.vertices[2].select = True
     obj_a3.data.vertices[2].co.x = 1282/4096
     obj_a3.data.vertices[2].co.z = -1449/4096
-    obj_a3.data.vertices[2].co.y = -1282/4096
+    obj_a3.data.vertices[2].co.y = -1282/4096"""
     bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.edge_face_add()
+    #bpy.ops.mesh.edge_face_add()
     bpy.ops.object.editmode_toggle()
